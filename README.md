95234cb16b392cf05ffb74eeeb10dff67f82a671
3209571656 <3209571656@qq.com>
2022-06-07 12:10:35 +0800

    v2.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# pop3 client
+
+this is a pop3 client by C
+
+## enviroment
+```
+linux
+libb64
+```
+
+```cmd
+gcc -o output pop.c -lb64
+
+./output
+```
--- pop.c
+++ pop.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <b64/cencode.h>
 #include <b64/cdecode.h>
-
+#include <regex.h>
 // PASS VPHCBMGOEYSSIRLW
 #define SIZE 100
 
@@ -27,17 +27,19 @@ void clrscr()
   system("@cls||clear");
 }
 
-char* send_command(int sockfd,char* command) {
+char *send_command(int sockfd, char *command)
+{
   char buf[1000000];
   memset(buf, 0, sizeof(buf));
   write(sockfd, command, strlen(command));
   read(sockfd, buf, sizeof(buf));
-  char * ret = malloc(strlen(buf) + 1);
+  char *ret = malloc(strlen(buf) + 1);
   strcpy(ret, buf);
   return ret;
 }
 
-void get_user(int sockfd) {
+void get_user(int sockfd)
+{
   char command[1024] = "USER ";
   char tmp[1024];
   printf("Enter your username: ");
@@ -48,10 +50,11 @@ void get_user(int sockfd) {
   memset(command, 0, sizeof(command));
 }
 
-void get_pass(int sockfd) {
+void get_pass(int sockfd)
+{
   char command[1024] = "PASS ";
   char tmp[1024];
-  char* password = getpass("Enter your password: ");
+  char *password = getpass("Enter your password: ");
   strcpy(tmp, password);
   strcat(tmp, "\r\n");
   strcat(command, tmp);
@@ -59,97 +62,126 @@ void get_pass(int sockfd) {
   memset(command, 0, sizeof(command));
 }
 
-void test_get_user(int sockfd) {
+void test_get_user(int sockfd)
+{
   send_command(sockfd, "USER lawted0605\r\n");
 }
 
-void test_get_pass(int sockfd) {
+void test_get_pass(int sockfd)
+{
   send_command(sockfd, "PASS VPHCBMGOEYSSIRLW\r\n");
 }
 
-void get_list(int sockfd) {
+void get_list(int sockfd)
+{
   char command[1024] = "LIST\r\n";
-  char* msg = send_command(sockfd, command);
+  char *msg = send_command(sockfd, command);
   printf("%s", msg);
   memset(command, 0, sizeof(command));
   go_back(sockfd);
 }
 
-void get_stat(int sockfd) {
+void get_stat(int sockfd)
+{
   char command[1024] = "STAT\r\n";
-  char* msg = send_command(sockfd, command);
+  char *msg = send_command(sockfd, command);
   printf("%s\n", msg);
   memset(command, 0, sizeof(command));
   go_back(sockfd);
 }
 
-char* decode(const char* input)
+char *decode(const char *input)
 {
-	/* set up a destination buffer large enough to hold the encoded data */
-	char* output = (char*)malloc(SIZE);
-	/* keep track of our decoded position */
-	char* c = output;
-	/* store the number of bytes decoded by a single call */
-	int cnt = 0;
-	/* we need a decoder state */
-	base64_decodestate s;
+  /* set up a destination buffer large enough to hold the encoded data */
+  char *output = (char *)malloc(SIZE);
+  /* keep track of our decoded position */
+  char *c = output;
+  /* store the number of bytes decoded by a single call */
+  int cnt = 0;
+  /* we need a decoder state */
+  base64_decodestate s;
 
-	/*---------- START DECODING ----------*/
-	/* initialise the decoder state */
-	base64_init_decodestate(&s);
-	/* decode the input data */
-	cnt = base64_decode_block(input, strlen(input), c, &s);
-	c += cnt;
-	/* note: there is no base64_decode_blockend! */
-	/*---------- STOP DECODING  ----------*/
+  /*---------- START DECODING ----------*/
+  /* initialise the decoder state */
+  base64_init_decodestate(&s);
+  /* decode the input data */
+  cnt = base64_decode_block(input, strlen(input), c, &s);
+  c += cnt;
+  /* note: there is no base64_decode_blockend! */
+  /*---------- STOP DECODING  ----------*/
 
-	/* we want to print the decoded data, so null-terminate it: */
-	*c = 0;
+  /* we want to print the decoded data, so null-terminate it: */
+  *c = 0;
 
-	return output;
+  return output;
 }
 
+int reg_matches(const char *str, const char *pattern)
+{
+    regex_t re;
+    int ret;
 
-void get_retr(int sockfd) {
+    if (regcomp(&re, pattern, REG_EXTENDED) != 0)
+        return 0;
+
+    ret = regexec(&re, str, (size_t) 0, NULL, 0);
+    regfree(&re);
+
+    if (ret == 0)
+        return 1;
+
+    return 0;
+}
+
+void get_retr(int sockfd)
+{
   char command[1024] = "RETR ";
   char file_num[1024];
   printf("Enter the file index: ");
   scanf("%s", file_num);
   strcat(command, file_num);
   strcat(command, "\r\n");
-  char* msg = send_command(sockfd, command);
-  printf("%s\n", msg);
-  // char * p, * tmp;
-  // p = strtok_r(msg, "\n", &tmp);
-  // int flag = 0;
-  // do {
-  //   if (flag) {
-  //     p = decode(p);
-  //   }
-  //   if (p[1] == '\n') {
-  //     flag = 1;
-  //   }
-  //   printf("%s\n", p);
-  // } while ((p = strtok_r(NULL, "\n", &tmp)) != NULL);
+  char *msg = send_command(sockfd, command);
+  char *p, *tmp;
+  p = strtok_r(msg, "\n", &tmp);
+  int flag = 0;
+  do
+  {
+    if (flag == 2) {
+      p = decode(p);
+      flag = 0;
+    }
+    if (strstr(p, "Content-Transfer-Encoding: base64") != NULL)
+    {
+      flag = 1;
+    }
+    if (*p == '\r' && flag == 1)
+    {
+      flag = 2;
+    }
+    printf("%s\n", p);
+  } while ((p = strtok_r(NULL, "\n", &tmp)) != NULL);
   memset(command, 0, sizeof(command));
   go_back(sockfd);
 }
 
-void save_file(char* str, char* filename) {
+void save_file(char *str, char *filename)
+{
   FILE *fp;
   fp = fopen(filename, "w");
   fprintf(fp, "%s", str);
   fclose(fp);
 }
 
-void download(int sockfd) {
+void download(int sockfd)
+{
   char command[1024] = "RETR ";
   char file_num[1024];
   printf("Enter the file index: ");
   scanf("%s", file_num);
   strcat(command, file_num);
   strcat(command, "\r\n");
-  char* msg = send_command(sockfd, command);
+  char *msg = send_command(sockfd, command);
   save_file(msg, strcat(file_num, ".eml"));
   memset(command, 0, sizeof(command));
   char command2[1024] = "DELE ";
@@ -159,8 +191,51 @@ void download(int sockfd) {
   go_back(sockfd);
 }
 
-void search(int sockfd) {
-
+void search(int sockfd)
+{
+  char command[1024] = "LIST\r\n";
+  char *msg = send_command(sockfd, command);
+  memset(command, 0, sizeof(command));
+  char *p, *tmp;
+  p = strtok_r(msg, "\n", &tmp);
+  int lines = 0;
+  do {
+    lines += 1;
+  } while ((p = strtok_r(NULL, "\n", &tmp)) != NULL);
+  lines -= 2;
+  for (int i = 0 ; i < lines ; i++)
+  {
+    char command2[1024] = "RETR ";
+    char file_num[1024];
+    sprintf(file_num, "%d", i + 1);
+    strcat(command2, file_num);
+    strcat(command2, "\r\n");
+    char *msg = send_command(sockfd, command2);
+    memset(command2, 0, sizeof(command2));
+    char *p, *tmp;
+    p = strtok_r(msg, "\n", &tmp);
+    int flag = 0;
+    do
+    {
+      if (flag == 2) {
+        p = decode(p);
+        flag = 0;
+      }
+      if (strstr(p, "Content-Transfer-Encoding: base64") != NULL)
+      {
+        flag = 1;
+      }
+      if (*p == '\r' && flag == 1)
+      {
+        flag = 2;
+      }
+      if (strstr(p, "Subject:") != NULL)
+      {
+        printf("%s\n", p);
+      }
+    } while ((p = strtok_r(NULL, "\n", &tmp)) != NULL);
+  }
+  go_back(sockfd);
 }
 
 void menu(int sockfd)
@@ -201,12 +276,11 @@ void menu(int sockfd)
     get_retr(sockfd);
     break;
   case 5:
-    printf("Search text in all mails");
-    search(sockfd);
+    printf("Search text in all mails\n");
     break;
   case 6:
-    printf("Display by subject");
-
+    printf("Display by subject\n");
+    search(sockfd);
     break;
   case 7:
     printf("Download the email\n");
@@ -216,7 +290,8 @@ void menu(int sockfd)
   }
 }
 
-void go_back(int sockfd) {
+void go_back(int sockfd)
+{
   int choice;
   printf(GRN "********************************************************\n" RESET);
   printf(RED "1.Quit\n" RESET);
@@ -235,13 +310,12 @@ void go_back(int sockfd) {
   }
 }
 
-
+// get content of email
 
 int main()
 {
   char buf[1024];
   printf("Welecom to pop3 client\n");
-  // send message by socket
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   struct sockaddr_in servaddr;
   if (sockfd < 0)
@@ -274,11 +348,10 @@ int main()
   get_user(sockfd);
   get_pass(sockfd);
 
-
   // test_get_user(sockfd);
   // test_get_pass(sockfd);
 
-  // clrscr();
+  clrscr();
   menu(sockfd);
   return 0;
 }
